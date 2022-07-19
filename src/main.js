3385a2929cbdbb966c2be6d54947bd64e04148ab
Paola Nuñez <98001281+Pdvnd@users.noreply.github.com>
2022-07-19 14:22:06 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -134,33 +134,73 @@ plantilla.addEventListener("load", complete)
 /*Videos*/
 const sliderContainer = document.querySelector("#slider");
 const currentContainer = document.querySelector("#current");
-const videosContainer = document.querySelector ("#videos-container");
-const bNext = document.querySelector ("#next");
-const bPrev = document.querySelector ("#prev");
+const videosContainer = document.querySelector("#videos-container");
+const bNext = document.querySelector("#next");
+const bPrev = document.querySelector("#prev");
 let current = 0;
 
-let videos = new XMLHttpRequest()
-videos.responseType="json"
-videos.open("GET", "videos.json")
-videos.send()
-videos.addEventListener("load", insertarVideos)
+const videos = [
+    {
+      "id": "TKvHU3Kpg2A"
+    },
+    {
+      "id": "vsAj692gebg"
+    },
+    {
+      "id": "WqTwaQzjYtg"
+    },
+    {
+      "id": "T03PxxuCfDA"
+    },
+    {
+        "id": "Jq_7fGb8ZDM"
+    },
+    {
+        "id": "4rKFkLeRF3Q"
+    }
+    ];
 
 bNext.addEventListener("click", (e) => {
-  current = current + 1  <= videos.length ? current + 1 : current;
-  insertarVideos(videos[current].id);
+  let changed = current;
+  current = current + 1 < videos.length ? current + 1 : current;
+  if (current !== changed) {
+    renderCurrentVideo(videos[current].id);
+  }
 });
 
 bPrev.addEventListener("click", (e) => {
-  current = current - 1 >= 0 ? current - 1 :current;
-  insertarVideos(videos[current].id);
+  let changed = current;
+  current = current - 1 >= 0 ? current - 1 : current;
+  if (current !== changed) {
+    renderCurrentVideo(videos[current].id);
+  }
 });
 
+renderCurrentVideo(videos[current].id);
+renderVideos();
 
+function renderCurrentVideo(id) {
+  currentContainer.innerHTML = `<iframe width="100%" height="600" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+}
 
-function insertarVideos (id){
-  const videos = id.currentTarget.response;{
-  for (const video of videos) {
-    currentContainer.innerHTML =(`
-    <iframe width="853" height="480" src="https://www.youtube.com/embed/${video.id}" title="🔥  Sitio Web con HTML y CSS de Halo usando Flex, dark mode y responsive design" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
-        
-  }}}
+function renderVideos() {
+  const html = videos.map((video, index) => {
+    return `
+    <div class="item">
+      <a href="#" data-id="${index}">
+        <img src="https://i3.ytimg.com/vi/${video.id}/mqdefault.jpg" />
+      </a>
+    </div>`;
+  });
+
+  videosContainer.innerHTML = html.join("");
+
+  document.querySelectorAll(".item a").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = +item.getAttribute("data-id");
+      current = id;
+      renderCurrentVideo(videos[current].id);
+    });
+  });
+}
